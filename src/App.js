13c639499ee3db92a3c2a96d6af18d6a7fb24269
Seth Purr <eric.purrington@gmail.com>
2021-01-08 19:03:09 -0700

    some ground work for components and gutted boilerplate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Groceries from "./components/Groceries";
+import Footer from "./components/Footer";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-
+        <Header />
+        <Groceries />
+        <Footer />
     </div>
   );
 }
-
-export default App;
